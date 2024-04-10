a65020dcba51e28bd9ee95faea732d87f343e35b
Vladislav Nikiforov <vnikif@ttu.ee>
2024-04-10 22:09:34 +0300

    Scenarios and several tests
    Wrote down several test scenarios in a TestScenarios.md file.
    
    Made a test that checks the filling of the calculator fields.
    
    Made a test that fills fields, clicks to calculate and checks the calculated value.
    
    Added method that finds the maximum input value and method that counts the number of characters.
    TODO: move it to separate class and continue with implementing the test scenarios.

---
+++ src/test/TestScenarios.md
@@ -0,0 +1,11 @@
+#Scenario 1: Entering data into the factoring calculator
+1. Open the factoring calculator page.
+2. Apply cookies.
+3. Enter the data (Invoice amount, Advance rate, Interest rate,
+   Payment term, Commission fee).
+4. Check that the entered data is displayed correctly.
+
+#Scenario 2: Checking the limits for maximum and minimum values.
+
+
+#Scenario 3: Validation of fields to accept only numeric values.
--- src/test/java/FactoringCalculatorUITests.java
+++ src/test/java/FactoringCalculatorUITests.java
@@ -1,7 +1,11 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.Test;
 
 import static com.codeborne.selenide.Selenide.*;
+import java.math.BigInteger;
+import java.util.Random;
+
 
 public class FactoringCalculatorUITests {
 
@@ -9,7 +13,70 @@ public class FactoringCalculatorUITests {
     public void basicTest() {
         open("https://www.swedbank.lt/business/finance/trade/factoring?language=ENG");
         $x("//button[@class='button ui-cookie-consent__accept-button']").click();
-        $x("//section[@class='frame']").shouldBe(Condition.visible);
+        $x("//ui-calculator[@state='hydrated']").shouldBe(Condition.visible);
+    }
+
+    @Test
+    public void fillAndDisplayTest() {
+        $x("//input[@id='D5']").setValue("999999").getValue().contains("999999");
+
+        SelenideElement selectD6 = $x("//select[@id='D6']");
+        selectD6.selectOption("80");
+        selectD6.getValue().contains("80");
+
+        $x("//input[@id='D7']").setValue("20").getValue().contains("20");
+
+        SelenideElement selectD8 = $x("//select[@id='D8']");
+        selectD8.selectOption("60");
+        selectD8.getValue().contains("60");
+
+        $x("//input[@id='D9']").setValue("999999").getValue().contains("999999");
+
+    }
+
+    @Test
+    public void fillCalc() {
+        $x("//input[@id='D5']").setValue("999999");
+        $x("//select[@id='D6']").selectOption("85");
+        $x("//input[@id='D7']").setValue("20");
+        $x("//select[@id='D8']").selectOption("90");
+        $x("//input[@id='D9']").setValue("999999");
+
+        $x("//button[@id='calculate-factoring']").click();
+        $x("//output[@id='result']").getValue().contains("10000022499.97");
+        System.out.println($x("//output[@id='result']").getValue());
+    }
+
+    @Test
+    public void maxInputValueCheck() {
+        System.out.println(countDigits(findMaxInputValue()));
+    }
+
+    public BigInteger findMaxInputValue() { // TODO: implement a separate class
+        int bitLength = 8;
+        Random random = new Random();
+        BigInteger value = new BigInteger(bitLength, random);
+        boolean errorFlag = false;
+        while (!errorFlag) {
+            try {
+                setInputValue(value);
+                errorFlag = $x("//ui-hint[@type='error']").exists();
+            } catch (Exception e) {
+                errorFlag = true;
+                break;
+            }
+            value = value.multiply(BigInteger.TEN);
+        }
+
+        return value.divide(BigInteger.TEN);
+    }
+
+    private void setInputValue(BigInteger value) {
+        $x("//input[@id='D5']").setValue(String.valueOf(value));
+    }
+
+    public int countDigits(BigInteger number) {
+        return number.toString().length();
     }
 
 }
